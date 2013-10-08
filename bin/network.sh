ddd0d4c14b4e072f1597ec89bb213eaa0ca2788b
Richard Su <rwsu@redhat.com>
2013-10-07 23:53:25 -0700

    Fixes to network.sh for dnsmasq and missing eth1 interface
    Puppet starts the dnsmasq service. The service needs to stopped
    to allow the default network to come up. The dnsmasq service will
    start back up with the default network.
    
    eth1 is set as the public_interface in the local heat metadata.
    Not all environments will have an eth1. Added some logic to
    change eth1 to eth0 if eth0 is the interface that is present.

--- bin/network.sh
+++ bin/network.sh
@@ -11,6 +11,16 @@ wait_for 12 10 ls /var/run/libvirt/libvirt-sock
 PUBLIC_INTERFACE=${PUBLIC_INTERFACE:-ucl0}
 
 sudo sed -i "s/bridge name='brbm'/bridge name='br-ctlplane'/" /opt/stack/tripleo-incubator/templates/brbm.xml
+
+# puppet will start dnsmasq service, need to stop it to allow
+# the default network to come up
+service nova-bm-dnsmasq stop
+
+# change interface in local heat metadata if eth0 is present
+if ifconfig | grep eth0; then
+    sed -i "s/\"public_interface\": \"eth1\"/\"public_interface\": \"eth0\"/g" /var/lib/heat-cfntools/cfn-init-data
+fi
+
 /opt/stack/tripleo-incubator/scripts/setup-network
 
 sudo ip link del $PUBLIC_INTERFACE || true
