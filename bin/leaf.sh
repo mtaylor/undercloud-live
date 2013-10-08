b5fcac18c8657988032570da5bb6641dbe3f8793
James Slagle <jslagle@redhat.com>
2013-10-08 09:30:23 -0400

    Add leaf.sh to install a leaf node.  install.sh is modified to just install a control node.  There is still a lot of code duplication between the two that can be refactored out.
    The long term idea is that install.sh will be just for the control node, and
    leaf.sh will be just for the leaf.  If you want an all-in-one, you'd run both.

--- bin/leaf.sh
+++ bin/leaf.sh
@@ -2,7 +2,7 @@
 
 set -eux
 
-if [ -f /opt/stack/undercloud-live/.install ]; then
+if [ -f /opt/stack/undercloud-live/.leaf ]; then
     echo install.sh has already run, exiting.
     exit
 fi
@@ -22,8 +22,7 @@ sudo pip install -U setuptools
 # pip thinks it's installed from then on out, even though it is not.
 sudo pip install pbr
 
-# qemu-img is still needed to convert disks when diskimage-builder is used
-sudo yum install -y python-lxml qemu-img git python-pip openssl-devel python-devel gcc audit python-virtualenv openvswitch python-yaml
+sudo yum install -y python-lxml libvirt-python libvirt qemu-img qemu-kvm git python-pip openssl-devel python-devel gcc audit python-virtualenv openvswitch python-yaml
 
 sudo mkdir -m 777 -p /opt/stack
 pushd /opt/stack
@@ -60,55 +59,31 @@ dib-elements -p diskimage-builder/elements/ tripleo-image-elements/elements/ \
     -b 15-fedora-remove-grub \
     -i
 dib-elements -p diskimage-builder/elements/ tripleo-image-elements/elements/ \
-    -e source-repositories boot-stack \
+    -e source-repositories nova-baremetal bm-dnsmasq neutron-network-node \
     -k extra-data \
     -i
 # rabbitmq-server does not start with selinux enforcing.
 # https://bugzilla.redhat.com/show_bug.cgi?id=998682
 dib-elements -p diskimage-builder/elements/ tripleo-image-elements/elements/ \
                 undercloud-live/elements \
-    -e boot-stack \
+    -e nova-baremetal bm-dnsmasq neutron-network-node \
        stackuser heat-cfntools \
-       undercloud-control-config selinux-permissive \
+       undercloud-leaf-config selinux-permissive \
     -k install \
     -i
 
 popd
 
-# sudo run from nova rootwrap complains about no tty
-sudo sed -i "s/Defaults    requiretty/# Defaults    requiretty/" /etc/sudoers
+# the current user needs to always connect to the system's libvirt instance
+# when virsh is run
+if [ ! -e /etc/profile.d/virsh.sh ]; then
+    sudo su -c "cat >> /etc/profile.d/virsh.sh <<EOF
 
-# Overcloud heat template
-sudo make -C /opt/stack/tripleo-heat-templates overcloud.yaml
+# Connect to system's libvirt instance
+export LIBVIRT_DEFAULT_URI=qemu:///system
 
-source /opt/stack/undercloud-live/bin/custom-network.sh
+EOF
+"
+fi
 
-# This is the "fake" interface needed for init-neutron-ovs
-PUBLIC_INTERFACE=${PUBLIC_INTERFACE:-ucl0}
-
-# These variables are meant to be overridden if they need to be changed.
-# If you're testing on a vm that is running on a host with the default
-# 192.168.122.1 network already defined, you will want to set environment
-# variables to override these.
-NETWORK=${NETWORK:-192.168.122.1}
-LIBVIRT_IP_ADDRESS=${LIBVIRT_IP_ADDRESS:-192.168.122.1}
-LIBVIRT_NETWORK_RANGE_START=${LIBVIRT_NETWORK_RANGE_START:-192.168.122.2}
-LIBVIRT_NETWORK_RANGE_END=${LIBVIRT_NETWORK_RANGE_END:-192.168.122.254}
-
-sudo sed -i "s/192.168.122.1/$LIBVIRT_IP_ADDRESS/g" /etc/libvirt/qemu/networks/default.xml
-sudo sed -i "s/192.168.122.2/$LIBVIRT_NETWORK_RANGE_START/g" /etc/libvirt/qemu/networks/default.xml
-sudo sed -i "s/192.168.122.254/$LIBVIRT_NETWORK_RANGE_END/g" /etc/libvirt/qemu/networks/default.xml
-
-# Modify config.json as necessary
-sudo sed -i "s/192.168.122.1/$NETWORK/g" /var/lib/heat-cfntools/cfn-init-data
-sudo sed -i "s/\"user\": \"stack\",/\"user\": \"$USER\",/" /var/lib/heat-cfntools/cfn-init-data
-sudo sed -i "s/eth1/$PUBLIC_INTERFACE/g" /var/lib/heat-cfntools/cfn-init-data
-
-sudo sed -i "s/192.168.122.1/$NETWORK/g" /opt/stack/os-config-applier/templates/var/opt/undercloud-live/masquerade
-
-# Need to get a patch upstream for this, but for now, just fix it locally
-# Run os-config-applier earlier in the os-refresh-config configure.d phase
-sudo mv /opt/stack/os-config-refresh/configure.d/50-os-config-applier \
-        /opt/stack/os-config-refresh/configure.d/40-os-config-applier
-
-touch /opt/stack/undercloud-live/.install 
+touch /opt/stack/undercloud-live/.leaf
