0366ca91570b077c726c6e487399b107964d10c7
James Slagle <jslagle@redhat.com>
2013-10-10 06:48:56 -0400

    Updated baremetal.sh

--- bin/baremetal.sh
+++ bin/baremetal.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-# Script to perform baremetal setup.
-# This can be run multiple times, but you must first delete your existing
-# baremetal nodes in nova with 'nova baremetal-node-delete'.
-
 set -eux
 
-# MACS must be set for setup-baremetal to work
-export MACS=$(/opt/stack/tripleo-incubator/scripts/get-vm-mac baremetal_0; /opt/stack/tripleo-incubator/scripts/get-vm-mac baremetal_1)
-# $TRIPLEO_ROOT is not true to the tripleo sense, but it's where
-# setup-baremetal look for the deploy kernel and ramfs.
-TRIPLEO_ROOT=/opt/stack/images /opt/stack/tripleo-incubator/scripts/setup-baremetal 1 2048 20 amd64 "$MACS" undercloud-live
+source /etc/sysconfig/undercloud-live-config
+source /etc/sysconfig/undercloudrc 
+
+# The NODE_* variables have sane defaults, but we need to exit if
+# $UNDERCLOUD_MACS is not defined.
+if [ -z "$UNDERCLOUD_MACS" ] then
+    echo \$UNDERCLOUD_MACS must be defined
+    exit 1
+fi
+
+TRIPLEO_ROOT=/home/jslagle/tripleo setup-baremetal $NODE_CPU $NODE_MEM $NODE_DISK $NODE_ARCH "$UNDERCLOUD_MACS" undercloud-leaf
