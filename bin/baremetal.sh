c8f730c0af343b20e2914f51600b467010feb60f
James Slagle <jslagle@redhat.com>
2013-10-10 12:24:15 -0400

    Syntax error and correct  path

--- bin/baremetal.sh
+++ bin/baremetal.sh
@@ -7,9 +7,9 @@ source /etc/sysconfig/undercloudrc
 
 # The NODE_* variables have sane defaults, but we need to exit if
 # $UNDERCLOUD_MACS is not defined.
-if [ -z "$UNDERCLOUD_MACS" ] then
+if [ -z "$UNDERCLOUD_MACS" ]; then
     echo \$UNDERCLOUD_MACS must be defined
     exit 1
 fi
 
-TRIPLEO_ROOT=/home/jslagle/tripleo setup-baremetal $NODE_CPU $NODE_MEM $NODE_DISK $NODE_ARCH "$UNDERCLOUD_MACS" undercloud-leaf
+TRIPLEO_ROOT=/opt/stack/images setup-baremetal $NODE_CPU $NODE_MEM $NODE_DISK $NODE_ARCH "$UNDERCLOUD_MACS" undercloud-leaf
