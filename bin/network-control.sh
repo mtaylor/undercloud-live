c98709feee96ecdc823e6d9dd913779b7389567d
James Slagle <jslagle@redhat.com>
2013-10-15 16:42:45 -0400

    Add a touched file network-control.sh so it only runs once.

--- bin/network-control.sh
+++ bin/network-control.sh
@@ -2,6 +2,10 @@
 
 set -eux
 
+if [ -f /opt/stack/undercloud-live/.network-control ]; then
+    exit
+fi
+
 source /opt/stack/undercloud-live/bin/common.sh
 source /etc/sysconfig/undercloud-live-config
 
@@ -12,3 +16,5 @@ TENANT_ID=$(keystone tenant-list | grep ' admin ' | awk '{print $2}')
 
 neutron net-create --tenant-id $TENANT_ID ctlplane --shared --provider:network_type flat --provider:physical_network ctlplane
 neutron subnet-create --tenant-id $TENANT_ID ctlplane 192.0.2.0/24
+
+sudo touch /opt/stack/undercloud-live/.network-control
