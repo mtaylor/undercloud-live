33daa80d53e2d7207f706731ef4df5a0da9165f8
Ben Nemec <bnemec@redhat.com>
2013-10-16 22:06:14 +0000

    Pass NovaComputeLibvirtType to Heat correctly

--- bin/deploy-overcloud.sh
+++ bin/deploy-overcloud.sh
@@ -10,5 +10,5 @@ source tripleo-overcloud-passwords
 export OVERCLOUD_LIBVIRT_TYPE=qemu
 
 heat stack-create -f /opt/stack/tripleo-heat-templates/overcloud.yaml \
-    -P "AdminToken=${OVERCLOUD_ADMIN_TOKEN};AdminPassword=${OVERCLOUD_ADMIN_PASSWORD};CinderPassword=${OVERCLOUD_CINDER_PASSWORD};GlancePassword=${OVERCLOUD_GLANCE_PASSWORD};HeatPassword=${OVERCLOUD_HEAT_PASSWORD};NeutronPassword=${OVERCLOUD_NEUTRON_PASSWORD};NovaPassword=${OVERCLOUD_NOVA_PASSWORD}${OVERCLOUD_LIBVIRT_TYPE}" \
+    -P "AdminToken=${OVERCLOUD_ADMIN_TOKEN};AdminPassword=${OVERCLOUD_ADMIN_PASSWORD};CinderPassword=${OVERCLOUD_CINDER_PASSWORD};GlancePassword=${OVERCLOUD_GLANCE_PASSWORD};HeatPassword=${OVERCLOUD_HEAT_PASSWORD};NeutronPassword=${OVERCLOUD_NEUTRON_PASSWORD};NovaPassword=${OVERCLOUD_NOVA_PASSWORD};NovaComputeLibvirtType=${OVERCLOUD_LIBVIRT_TYPE}" \
     overcloud
