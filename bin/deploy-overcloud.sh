ab98d8aa56095d1e03e686cbfe71759ed2db1e7a
James Slagle <jslagle@redhat.com>
2013-10-17 12:33:28 -0400

    Set $OVERCLOUD_LIBVIRT_TYPE in /etc/sysconfig/undercloud-live-config.

--- bin/deploy-overcloud.sh
+++ bin/deploy-overcloud.sh
@@ -3,11 +3,10 @@
 set -eux
 
 source /etc/sysconfig/undercloudrc
+source /etc/sysconfig/undercloud-live-config
 
 /opt/stack/tripleo-incubator/scripts/setup-overcloud-passwords -o
 source tripleo-overcloud-passwords
-
-export OVERCLOUD_LIBVIRT_TYPE=qemu
 
 heat stack-create -f /opt/stack/tripleo-heat-templates/overcloud.yaml \
     -P "AdminToken=${OVERCLOUD_ADMIN_TOKEN};AdminPassword=${OVERCLOUD_ADMIN_PASSWORD};CinderPassword=${OVERCLOUD_CINDER_PASSWORD};GlancePassword=${OVERCLOUD_GLANCE_PASSWORD};HeatPassword=${OVERCLOUD_HEAT_PASSWORD};NeutronPassword=${OVERCLOUD_NEUTRON_PASSWORD};NovaPassword=${OVERCLOUD_NOVA_PASSWORD}${OVERCLOUD_LIBVIRT_TYPE}" \
