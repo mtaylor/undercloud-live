b19d74d29be9e02cc130716db2033bd5613a1e7e
Ben Nemec <bnemec@redhat.com>
2013-10-18 20:06:41 +0000

    Set git config for cherry-picks

--- bin/install.sh
+++ bin/install.sh
@@ -47,6 +47,8 @@ popd
 
 git clone https://github.com/openstack/diskimage-builder.git
 pushd diskimage-builder
+git config user.email "undercloud-live@example.com"
+git config user.name "Undercloud Live"
 git checkout 9211a7fecbadc13e8254085133df1e3b53f150d8
 git fetch https://review.openstack.org/openstack/diskimage-builder refs/changes/30/46230/1 && git cherry-pick -x FETCH_HEAD
 git fetch https://review.openstack.org/openstack/diskimage-builder refs/changes/21/52321/3 && git cherry-pick -x FETCH_HEAD
